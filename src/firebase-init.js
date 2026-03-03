1f2c9332b70e5ace122c0310effdd0b4067202ce
Alok Priyadarshi <alokpriyadarshi618@gmail.com>
2026-03-03 12:46:08 +0530

    Initial commit

---
+++ src/firebase-init.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBoqrkjjOaAly5MD2Cd_hAldGLz6isMThM",
+    authDomain: "WhatsappX-2-372312.firebaseapp.com",
+    projectId: "WhatsappX-2-372312",
+    storageBucket: "WhatsappX-2-372312.appspot.com",
+    messagingSenderId: "225921394239",
+    appId: "1:225921394239:web:66e9a0b9cd976ec204ba8a"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  export { db }
